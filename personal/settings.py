a020bcd9d376ea1d05d35ea13c5eb03e559473d4
florimondmanca <florimond.manca@gmail.com>
2018-07-09 00:16:59 +0100

    fix staticfiles config

--- personal/settings.py
+++ personal/settings.py
@@ -180,18 +180,9 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'staticfiles'),
-]
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-
-
-STATICFILES_FINDERS = [
-    'django.contrib.staticfiles.finders.FileSystemFinder',
-    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
-]
 
 
 # Markdownx settings
--- personal/wsgi.py
+++ personal/wsgi.py
@@ -1,17 +1,10 @@
-"""
-WSGI config for personal project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/2.0/howto/deployment/wsgi/
-"""
+"""WSGI configuration."""
 import os
 
 from django.core.wsgi import get_wsgi_application
 
 from whitenoise import WhiteNoise
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "personal.settings")
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'personal.settings')
 application = get_wsgi_application()
 application = WhiteNoise(application)
