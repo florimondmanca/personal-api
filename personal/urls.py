8d59a46b81fa93fdfd9650dbeec75e672991611c
florimondmanca <florimond.manca@gmail.com>
2018-07-09 01:37:08 +0100

    add index url

--- personal/urls.py
+++ personal/urls.py
@@ -1,5 +1,6 @@
 """URL configuration."""
 
+from django.views.generic.base import RedirectView
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
@@ -9,6 +10,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('markdownx/', include('markdownx.urls')),
     path('api/', include('api.urls')),
+    path('', RedirectView.as_view(url='admin', permanent=False), name='index')
 ]
 
 if settings.DEBUG:
