dba6f581e290fbd0e25cf28154bc4edaccb78c5e
florimondmanca <florimond.manca@gmail.com>
2018-07-14 18:06:02 +0100

    update port in captain-definition

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,4 +2,4 @@ echo "Running migrations..."
 pipenv run python manage.py migrate
 
 echo "Starting Gunicorn..."
-exec pipenv run gunicorn -b 0.0.0.0:8000 personal.wsgi:application
+exec pipenv run gunicorn -b 0.0.0.0:80 personal.wsgi:application
