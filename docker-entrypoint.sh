651b86e5675ce745cb65aca562dd780dbfbc6f13
florimondmanca <florimond.manca@gmail.com>
2018-07-21 20:54:33 +0100

    revert debug changes

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,5 +2,4 @@ echo "Running migrations..."
 pipenv run python manage.py migrate
 
 echo "Starting Gunicorn..."
-exec pipenv run gunicorn -b localhost:8000 personal.wsgi:application
-# exec pipenv run gunicorn -b 0.0.0.0:80 personal.wsgi:application
+exec pipenv run gunicorn -b 0.0.0.0:80 personal.wsgi:application
