0e3378a5cbc03156181f4d64c64646e0a5c4025e
florimondmanca <florimond.manca@gmail.com>
2018-08-15 22:55:15 +0100

    support update of image_url

--- blog/serializers.py
+++ blog/serializers.py
@@ -8,6 +8,28 @@ from rest_framework.validators import UniqueValidator
 from .models import Post
 
 
+class ImageUrlField(serializers.Field):
+    """Custom field for a post's image URL."""
+
+    def get_attribute(self, obj: Post) -> Post:
+        """Pass the post object itself to `to_representation`."""
+        return obj
+
+    def to_representation(self, value: Post) -> str:
+        """Return the absolute URL to the post's image."""
+        relative_url = value.get_image_url()
+        if relative_url is None:
+            return None
+        request = self.context.get('request')
+        if not request:  # cannot build full URL without request
+            return relative_url
+        return request.build_absolute_uri(relative_url)
+
+    def to_internal_value(self, data: str) -> str:
+        """Return the incoming image URL itself."""
+        return data
+
+
 class PostSerializer(serializers.HyperlinkedModelSerializer):
     """Serializer for blog posts."""
 
@@ -15,18 +37,8 @@ class PostSerializer(serializers.HyperlinkedModelSerializer):
         max_length=Post.SLUG_MAX_LENGTH,
         validators=[UniqueValidator(queryset=Post.objects.all())]
     )
-    image_url = serializers.SerializerMethodField()
+    image_url = ImageUrlField(required=False)
     description = serializers.SerializerMethodField()
-
-    def get_image_url(self, post: Post) -> Union[str, None]:
-        """Return the full absolute URL to the post's image, or None."""
-        relative_url = post.get_image_url()
-        if relative_url is None:
-            return None
-        request = self.context.get('request')
-        if not request:
-            return relative_url
-        return request.build_absolute_uri(relative_url)
 
     def get_description(self, post: Post) -> str:
         """Return the post's description or its preview if not set."""
--- tests/test_blog/test_post.py
+++ tests/test_blog/test_post.py
@@ -209,6 +209,24 @@ class PostUpdateTest(APITestCase):
         response = self.client.put(url, data=payload, format='json')
         self.assertEqual(response.status_code, 200)
 
+        post = Post.objects.get(pk=self.post.pk)  # reload from DB
+        for field, value in payload.items():
+            self.assertEqual(value, getattr(post, field))
+
+    def test_can_update_image_url(self):
+        initial_url = 'https://fakeimg.pl/128/'
+        new_url = 'https://fakeimg.pl/256/'
+
+        post = PostFactory.create(image_url=initial_url)
+
+        url = f'/api/posts/{post.slug}/'
+        data = {'image_url': new_url}
+        response = self.client.patch(url, data=data, format='json')
+
+        self.assertEqual(response.status_code, 200)
+        post = Post.objects.get(pk=post.pk)  # reload from DB
+        self.assertEqual(post.image_url, new_url)
+
 
 @authenticated
 class PostPublishTest(APITestCase):
