bc02a6ce3bb2722bda8c8b438fa22997df639bab
florimondmanca <florimond.manca@gmail.com>
2018-09-30 23:49:27 +0200

    Fix bug when image_url was null in post update

--- tests/test_blog/test_post_update.py
+++ tests/test_blog/test_post_update.py
@@ -14,19 +14,32 @@ class PostUpdateTest(APITestCase):
     def setUp(self):
         self.post = PostFactory.create()
 
-    def test_update(self):
-        payload = {
+    def _get_default_payload(self):
+        return {
             'title': 'Did everyone forget about apple juice?',
             'slug': 'did-everyone-forget-about-apple-juice',
             'content': 'Maybe.',
         }
+
+    def _perform(self, **kwargs):
+        payload = {
+            **self._get_default_payload(),
+            **kwargs,
+        }
         url = f'/api/posts/{self.post.slug}/'
         response = self.client.put(url, data=payload, format='json')
-        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.status_code, 200, response.data)
+        return response
 
+    def test_update(self):
+        payload = self._get_default_payload()
+        self._perform(**payload)
         post = Post.objects.get(pk=self.post.pk)  # reload from DB
         for field, value in payload.items():
             self.assertEqual(value, getattr(post, field))
+
+    def test_image_url_can_be_null(self):
+        self._perform(image_url=None)
 
     def test_can_update_image_url(self):
         initial_url = 'https://fakeimg.pl/128/'
